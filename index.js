faaa59df35b41e619c621f99f5c88d49e33c4c7b
Cecily Grossmann <grssmnn2@gmail.com>
2018-02-03 15:33:44 -0600

    added required files, installed prompt and inquirer

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 // this file requires Word.js
 var Word = require("Word.js");
+// require inquirer package
+var inquirer = require('inquirer');
+// require prompt package
+var prompt = require('prompt');
 
 // randomly select a word and use Word constructor to store value
 
