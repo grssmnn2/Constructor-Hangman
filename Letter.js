5c427b8cc1f16f600fe42503323c199a8c8fc874
Cecily Grossmann <grssmnn2@gmail.com>
2018-02-08 10:38:29 -0600

    blanks still not showing

--- Letter.js
+++ Letter.js
@@ -1,27 +1,32 @@
 function Letter(letter) {
-    // string value to hold character for letter
+    // string value to hold letter in word selected
     this.letter = letter;
     // boolean value to store if letter has been guessed yet (initially false)
     this.letterGuessed = false;
     // returns letter if guessed is true, or shows all underscores if guessed is false
+    this.guess = function (guess){
+        // if guess matches the letter in the word
+        if(this.letter===guess){
+            // changer letterGuessed to true
+            this.letterGuessed=true;
+        }
+    };
     this.returnLetter = function (){
         console.log(this.letter);
+        // if the letter has been guessed
         if (this.letterGuessed){
+            // display it
             return this.letter;
             
         }else{
+            // else show dash
             return "-";
         }
     };
 
-    this.guess = function (character){
-        // if letter matches character
-        if(this.letter===character){
-            this.letterGuessed=true;
-        }
-    };
+   
 };
-// EXPORT MODULE anything Word.js needs
+
 module.exports = {
     Letter: Letter
 };
--- Word.js
+++ Word.js
@@ -10,22 +10,22 @@ function Word (randomWord) {
     this.randomWord = randomWord;
     // array of letters that will hold either dashes or letters
     this.letters = [];
-    // function to return a string representing word
-    this.wordAsString = function(){
         // initially array holds as many dashes as letters in word
         for (var i = 0; i<this.randomWord.length; i++){
-            Letter.returnLetter(this.randomWord[i]);
             // to use constructor on other page, enter require variable.constructor
-            this.letters.push(new Letter.Letter(this.randomWord[i]));            
-            // if a letter in the randomword equals a letter from user, display that letter in array
-            // otherwise display -, this comes from testing each letter in Letter constructor
-            }
-        };
+            this.letters.push(new Letter.Letter(this.randomWord[i])); 
+            }        
     // function to take character argument and use letter.js func on each letter obj
     this.check = function(guess){
-       Letter.guess(guess);
+       Letter.Letter(guess);
     };
 };
+// auto run toString by using prototype function
+Word.prototype.toString = function(){
+    for (var i=0; i<letters.length; i++){
+        console.log(letters.join(""));
+    }
+}
 
 // var object = new Letter("l", true);
 // Letter.returnLetter();
