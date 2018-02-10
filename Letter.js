8c78e89eef569fd6d45ff570068b656c38978ffa
Cecily Grossmann <grssmnn2@gmail.com>
2018-02-09 19:32:44 -0600

    updated readme

--- Letter.js
+++ Letter.js
@@ -3,26 +3,24 @@ function Letter(letter) {
     this.letter = letter;
     // boolean value to store if letter has been guessed yet (initially false)
     this.letterGuessed = false;
-    this.guess = function (guess) {
+    this.guess = function(guess){
+    if (this.letter === guess) {
         // if guess matches the letter in the word
-        if (this.letter === guess) {
-            // change letterGuessed to true
+        // change letterGuessed to true
             this.letterGuessed = true;
-        }
-    };
+        }  
+    }  
     // returns letter if guessed is true, or shows all underscores if guessed is false
     this.returnLetter = function () {
         // if the letter has been guessed
         if (this.letterGuessed) {
             // display it
             return this.letter;
-        } else {
-            // else show dash
-            return "-";
-        }
+        } // else show dash
+            return " - ";        
     };
 };
-
-
+var cat = new Letter("a");
+cat.guess();
 module.exports = Letter;
 
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Constructor-Hangman
 A Node Application Hangman Game
+Once prompted, start typing letters to guess the word represented by dashes.
+Dashes will change to letters once guessed correctly.
+Win the game by guessing the entire word.
